0975b9dc078f22c7947130b55e145240a71380b8
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-04-27 21:40:45 +0800

    Update snow.py

--- snow.py
+++ snow.py
@@ -2,13 +2,14 @@ import turtle
 scr = turtle.Screen()
 t=turtle.Turtle()
 t.speed(10)
-n,stage=150,4 #ubah stage untuk bentuk
+n,stage=150,4
+b=3 #ubah branch untuk bentuk lain2
 def star(h,s):
 	if s>0:
-		for i in range(stage):
+		for i in range(3):
 			t.fd(h)
 			star(h/2.5,s-1)
 			t.bk(h)
-			t.rt(360//stage)
+			t.rt(360//3)
 star(n,stage)
 turtle.mainloop()
